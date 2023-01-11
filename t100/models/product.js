871a717d5576b04433ebd77074fc04fd060933e6
Kanji <kanji.neto-ext@ab-inbev.com>
2023-01-11 15:27:29 -0800

    closing t102

---
+++ t100/models/product.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const products = [];
+
+module.exports =  class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        // products.push(this);
+        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+
+            if(!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {    
+                console.log(err);
+            });
+        });
+
+        
+    }
+
+    static fetchAll(cb) {
+        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+
+            if(err) {
+                cb([])
+            }
+
+            cb(JSON.parse(fileContent));
+            
+        });
+    }
+}
