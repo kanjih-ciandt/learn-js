a68efae1201deac3df47f746791e5fc450bf95d9
Kanji <kanji.neto-ext@ab-inbev.com>
2023-01-12 09:15:26 -0800

    closing t107

---
+++ t107/apps.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRouters = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const systemController = require('./controllers/system');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin',adminRouters);
+app.use(shopRoutes);
+
+app.use(systemController.pageNotFound);
+
+app.listen(3000);
