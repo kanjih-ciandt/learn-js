450517a369bda6c6737db964221b4c8027951bb8
Kanji <kanji.neto-ext@ab-inbev.com>
2023-01-12 17:33:14 -0800

    t115

--- t115/app.js
+++ t115/app.js
@@ -1,26 +1,24 @@
 const path = require('path');
-const http = require('http');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRouters = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
-const systemController = require('./controllers/system');
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin',adminRouters);
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
-app.use(systemController.pageNotFound);
+app.use(errorController.get404);
 
 app.listen(3000);
