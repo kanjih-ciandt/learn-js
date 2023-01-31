92579a832d05020167d9c347e4ebc0c0eb82b637
Kanji <kanji.neto-ext@ab-inbev.com>
2023-01-31 12:41:55 -0800

    t175

--- t149/controllers/shop.js
+++ t149/controllers/shop.js
@@ -113,7 +113,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders({includes: ['products']})
+  req.user.getOrders({include: ['products']})
   .then(orders => {
     res.render('shop/orders', {
       path: '/orders',
