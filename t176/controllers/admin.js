642857e62546de1f55e10c1527a26257b1370aa1
Kanji <kanji.neto-ext@ab-inbev.com>
2023-02-03 16:48:01 -0800

    t185

--- t176/controllers/admin.js
+++ t176/controllers/admin.js
@@ -48,26 +48,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  }).then(result => {
+  const product = new Product(title, price, description, imageUrl);
+  product.save()
+  .then(result => {
     console.log('Created Product');
     res.redirect('/admin/products');
   }).catch(error => console.log(error));
-  
-  // Product.create({
-  //   title: title,
-  //   price: price,
-  //   imageUrl: imageUrl,
-  //   description: description
-  // }).then(result => {
-  //   console.log(result);
-  // }).catch(error => console.log(error));
-
-  
 };
 
 exports.postEditProduct = (req, res, next) => {
