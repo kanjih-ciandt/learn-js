6a84daada1d91dc4ce32a90a2208a0f1e3f67a63
Kanji <kanji.neto-ext@ab-inbev.com>
2023-02-07 10:33:58 -0800

    t194

--- t176/models/user.js
+++ t176/models/user.js
@@ -1,22 +1,39 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDB = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name:{
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email; 
   }
-});
+
+  save() {
+    const db = getDB();
+    let dbOp;
+    if(this._id) {
+      dbOp = db.collection('users')
+      .updateOne({_id: this._id}, {$set: this});
+    } else {
+      dbOp = db.collection('users').insertOne(this);
+      
+    }
+    return dbOp.then(result => {
+      console.log(result);
+    }).catch(err => console.log(err));
+  }
+
+  static findById(userId) {
+    const db = getDB();
+    return db.collection('users').findOne({_id: new mongodb.ObjectId(userId)});
+  }
+
+  static fetchAll() {
+    const db = getDB();
+    return db.collection('users').find().toArray().then(users => {
+      console.log(users);
+      return users;
+    }).catch(error => console.log(error));
+  }
+}
 
 module.exports = User;
