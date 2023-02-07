bc257b30cb50e8e0ac9d4c7baf07e6179e052bdf
Kanji <kanji.neto-ext@ab-inbev.com>
2023-02-07 13:46:08 -0800

    t196

--- t176/models/user.js
+++ t176/models/user.js
@@ -2,9 +2,11 @@ const mongodb = require('mongodb');
 const getDB = require('../util/database').getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email; 
+    this.cart = cart; // {items: []};
+    this.__id = id;
   }
 
   save() {
@@ -20,6 +22,22 @@ class User {
     return dbOp.then(result => {
       console.log(result);
     }).catch(err => console.log(err));
+  }
+
+  addToCart(product) {
+    // const cartProductIndex = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    product.quantity = 1;
+    const updatedCartItems = {items: [{... product, quantity: 1}]};
+    const db = getDB();
+    return db
+      .collection('users')
+      .updateOne(
+        {_id: new mongodb.ObjectId(this._id)}, 
+        {$set: {cart: updatedCartItems}}
+      );
+    
   }
 
   static findById(userId) {
