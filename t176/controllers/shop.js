7631f873d5e2f2c74bc0624f9f007c4e3d1bb8db
Kanji <kanji.neto-ext@ab-inbev.com>
2023-02-07 17:12:06 -0800

    t201

--- t176/controllers/shop.js
+++ t176/controllers/shop.js
@@ -53,50 +53,16 @@ exports.postCart = (req, res, next) => {
     return req.user.addToCart(product);
   }).then( result => {
     console.log(result);
-    // res.redirect('/cart');
+    res.redirect('/cart');
   }).catch(error => console.log(error));
-  
-
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user.getCart().then(cart => {
-  //   fetchedCart = cart;
-  //   return cart.getProducts({where: {id: productId}});
-  // }).then(products => {
-  //   let product;
-  //   if (products.length > 0) {
-  //     product = products[0];
-  //   }
-    
-  //   if (product) {
-  //     const oldQuantity = product.cartItem.quantity;
-  //     newQuantity = oldQuantity + 1;
-  //     return product;
-      
-  //   }
-  //   return Product.findByPk(productId);
-    
-  // }).then(product => {
-  //   return fetchedCart.addProduct(product, {
-  //     through: {quantity: newQuantity}
-  //   });
-  // }).then(() => {
-  //   res.redirect('/cart');
-  // }).catch(error => console.log(error));
-
-  
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
-  req.user.getCart().then(cart => {
-    return cart.getProducts({where: {id: productId}});
-  } ).then(products => {
-    const product = products[0];
-    return product.cartItem.destroy();
-  } ).then(result => {
+  Product.deleteById(productId).then( result => {
+    console.log('Deleted product');
     res.redirect('/cart');
-  } ).catch(error => console.log(error)); 
+  }).catch(error => console.log(error));
 };
 
 exports.getOrders = (req, res, next) => {
--- t176/models/user.js
+++ t176/models/user.js
@@ -49,6 +49,19 @@ class User {
     
   }
 
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDB();
+    return db
+      .collection('users')
+      .updateOne(
+        {_id: new mongodb.ObjectId(this._id)},
+        {$set: {cart: {items: updatedCartItems}}}
+      );
+  }
+
   getCart() {
     const db = getDB();
 
