28f71e9d10431c2b641b467a5d891b0f807ae375
Kanji <kanji.neto-ext@ab-inbev.com>
2023-02-14 10:11:49 -0800

    t213

--- t209/models/product.js
+++ t209/models/product.js
@@ -1,56 +1,22 @@
-// const mongodb = require('mongodb');
-// const getDB = require('../util/database').getDb;
+const mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
 
-// class Product {
-//   constructor(title, imageUrl, description, price, id, userId) {
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//     this._id = id ? new mongodb.ObjectId(id) : null;
-//     this.userId = userId ? new mongodb.ObjectId(userId) : null;
-//   }
-
-//   save(){
-//     const db = getDB();
-//     let dbOp;
-//     if(this._id) {
-//       dbOp = db.collection('products')
-//       .updateOne({_id: this._id}, {$set: this});
-//     } else {
-//       dbOp = db.collection('products').insertOne(this);
-      
-//     }
-//     return dbOp.then(result => {
-//       console.log(result);
-//     }).catch(err => console.log(err));
-    
-
-//   }
-
-//   static fetchAll(){
-//     const db = getDB();
-//     return db.collection('products').find().toArray().then(products => {
-//       console.log(products);
-//       return products;
-//     }).catch(error => console.log(error));
-//   }
-
-//   static findById(prodId){
-//     const db = getDB();
-//     return db.collection('products').find({_id: new mongodb.ObjectId(prodId)}).next().then(product => {
-//       console.log(product);
-//       return product;
-//     }).catch(error => console.log(error));
-//   }
-
-//   static deleteById(prodId){
-//     const db = getDB();
-//     return db.collection('products').deleteOne({_id: new mongodb.ObjectId(prodId)}).then(result => {
-//       console.log('Deleted');
-//     }).catch(error => console.log(error));
-//   }
-// }
-
-// module.exports = Product;
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+});
