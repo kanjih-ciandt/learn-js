9d6aa8984518071d446bfdd05934d54f0b52dc1e
Kanji Hara Neto <hkanjih@gmail.com>
2023-05-02 08:49:35 -0700

    WIP Section 13 - Finished unit 227

--- t209/controllers/shop.js
+++ t209/controllers/shop.js
@@ -68,14 +68,14 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders()
-  .then(orders => {
-    res.render('shop/orders', {
-      path: '/orders',
-      pageTitle: 'Your Orders',
-      orders: orders
-    });
-  }).catch(error => console.log(error));
+  Order.find({'user.userId':req.user._id})
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    }).catch(error => console.log(error));
   
 };
 
