cbb1e5bb8c5016afd6c0a10d7e90fbfb84305ee6
Kanji Hara Neto <hkanjih@gmail.com>
2023-05-02 07:50:05 -0700

    WIP Section 13 - Finished unit 224

--- t209/controllers/shop.js
+++ t209/controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-// const Order = require('../models/order');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -80,8 +80,21 @@ exports.getOrders = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  req.user.addOrder()
-  .then(result => {
+  req.user
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items.map( i =>{
+        return {quantity: i.quantity, product: i.productId};
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      order.save();
+    }).then(result => {
     res.redirect('/orders');
   }).catch(error => console.log(error));
 };
